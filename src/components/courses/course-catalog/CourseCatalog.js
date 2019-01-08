f3e5029dac70ecbbbea6542d966acab449f284df
Scott Reyell <smreyell@gmail.com>
2019-01-08 15:38:55 -0800

    redux dev tools added again

--- src/components/courses/course-catalog/CourseCatalog.js
+++ src/components/courses/course-catalog/CourseCatalog.js
@@ -40,6 +40,7 @@ function truncate(str, no_words) {
                         <img className="list-img" src={require('../../../img/course-img.jpg')} alt="course thumbnail"/>
                         {/* <img className="list-img" src="https://source.unsplash.com/random/300x200" alt="course thumbnail"/> */}
                         <h2 className="list-item-title">{course.title}</h2>
+                       
                         <p className="list-item-description">{truncate(course.description,40)}</p> 
                         <div classname="">
                           <Link to={`/course/${course._id}`} className="btn btn-success course-list-item-btn">
