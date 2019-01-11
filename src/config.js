a92330884b56285d0d4f2839e84a566dc7757787
Scott Reyell <smreyell@gmail.com>
2019-01-10 23:22:11 -0800

    switched back to local host

--- src/config.js
+++ src/config.js
@@ -1,2 +1,7 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://enigmatic-reaches-73699.herokuapp.com/api';
+    process.env.REACT_APP_API_BASE_URL || 'https://localhost:8080/api';
+
+
+
+// export const API_BASE_URL =
+// process.env.REACT_APP_API_BASE_URL || 'https://enigmatic-reaches-73699.herokuapp.com/api';
