1f524439ddb03290a0a2bec87167c9e68c02c329
Scott Reyell <smreyell@gmail.com>
2019-01-10 23:38:03 -0800

    added favicon

--- src/config.js
+++ src/config.js
@@ -1,7 +1,3 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://localhost:8080/api';
+    process.env.REACT_APP_API_BASE_URL || 'https://enigmatic-reaches-73699.herokuapp.com/api';
 
-
-
-// export const API_BASE_URL =
-// process.env.REACT_APP_API_BASE_URL || 'https://enigmatic-reaches-73699.herokuapp.com/api';
