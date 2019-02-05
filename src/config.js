d6bc5dabf6fdf2bb612671f0ea498c40721e1180
Scott Reyell <smreyell@gmail.com>
2019-02-05 15:47:46 -0800

    changed api back to heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://localhost:8080/api';
+    process.env.REACT_APP_API_BASE_URL || 'https://salty-retreat-90605.herokuapp.com/api';
 
 // export const API_BASE_URL =
-//     process.env.REACT_APP_API_BASE_URL || 'https://salty-retreat-90605.herokuapp.com/api';
-
+//     process.env.REACT_APP_API_BASE_URL || 'https://localhost:8080/api';
 
