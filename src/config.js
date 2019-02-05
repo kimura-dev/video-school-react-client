295f5946d767fe09a5741c24435b8bc52371dc2c
Scott Reyell <smreyell@gmail.com>
2019-02-05 12:12:54 -0800

    changed api server in config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://enigmatic-reaches-73699.herokuapp.com/api';
+    process.env.REACT_APP_API_BASE_URL || 'https://salty-retreat-90605.herokuapp.com/api';
 
